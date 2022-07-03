b2b11c872721012fdd02b2171c5aaff153b60d79
dogancan.okur <dogancan.okur@gmail.com>
2022-07-04 00:15:44 +0300

    init project

---
+++ src/test/java/com/dogancanokur/microservices1product/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogancanokur.microservices1product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
